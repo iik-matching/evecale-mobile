cb4376af473d53994798f7ace65b2c15f0db2b29
入江健太 <>
2023-09-09 18:34:21 +0900

    カレンダー実装完了

--- components/Cell.tsx
+++ components/Cell.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
-import {StyleSheet, View, Button} from 'react-native';
+import {StyleSheet, Button, Text, TouchableOpacity, View} from 'react-native';
 
 interface CellProps {
   dayNum: number;
 }
 
-const handleButtonPress = () => {
+const cellTap = () => {
   console.log('ボタンが押されました');
 };
 
 const Cell: React.FC<CellProps> = ({dayNum}) => {
   return (
     <View style={styles.cell}>
-      <Button title={`${dayNum}`} onPress={handleButtonPress} />
+      <Text style={styles.textDay}>{`${dayNum}`}</Text>
+      <TouchableOpacity style={styles.viewCell} onPress={cellTap}>
+        <View style={styles.row}>
+          <Text style={styles.textRow} numberOfLines={1} ellipsizeMode="clip">
+            ELLEGARDEN
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.textRow} numberOfLines={1} ellipsizeMode="clip">
+            oneokRock
+          </Text>
+        </View>
+        <View style={styles.row}>
+          <Text style={styles.textRow} numberOfLines={1} ellipsizeMode="clip">
+            uverworld
+          </Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -23,6 +40,28 @@ const styles = StyleSheet.create({
     margin: 3,
     borderWidth: 1,
     backgroundColor: 'white',
+    textAlign: 'center',
+  },
+  textDay: {
+    backgroundColor: 'green',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  viewCell: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+    flex: 1,
+  },
+  row: {
+    backgroundColor: 'yellow',
+    borderRadius: 4,
+    borderWidth: 1,
+    marginBottom: 1,
+  },
+  textRow: {
+    paddingHorizontal: 3,
+    fontWeight: 'bold',
+    fontSize: 10,
   },
 });
 
